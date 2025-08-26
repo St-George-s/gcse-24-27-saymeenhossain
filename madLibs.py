13ebe4c3b81bbb9c30d7ecb6bbbee22749711734
saymeenhossain <hos008@stge.org.uk>
2025-08-26 09:23:41 +0000

    started on madLibs

---
+++ madLibs.py
@@ -0,0 +1,7 @@
+name = input("Enter a name: ")
+age = int(input("Enter an age: "))
+food = input("Enter a food: ")
+place = input("Enter a place: ")
+song = input("Enter a song: ")
+
+print(name, "is", age, "years old and loves eating", food, "and lives in", place, "likes the song", song)
