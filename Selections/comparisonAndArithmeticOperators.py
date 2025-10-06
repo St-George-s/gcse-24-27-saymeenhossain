0685da351c7c72a373c5b1828362ed02f62abb43
saymeenhossain <hos008@stge.org.uk>
2025-10-06 18:28:28 +0000

    arithnetuc

---
+++ Selections/comparisonAndArithmeticOperators.py
@@ -0,0 +1,35 @@
+#Question 1
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+if number1 == number2:
+    print("The numbers are equal")
+else:
+    print("The numbers are not equal")
+
+#Question 2 
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+if number1 > number2:
+    print(f"The larger number is number1")
+else:
+    print(f"The larger number is number2")
+
+#Question 3 
+
+#Question 4
+number = float(input("Enter a number: "))
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+
+#Question 5
+length = float(input("Enter length of rectangle: "))
+width = float(input("Enter width of rectangle: "))
+area = length * width
+
+
+#Question 6
