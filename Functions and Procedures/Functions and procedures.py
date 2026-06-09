64301561f8f6c2ce10d7a0c9fb226e9fe43124e7
saymeenhossain <hos008@stge.org.uk>
2026-06-09 09:26:08 +0000

    funcs and proc

---
+++ Functions and Procedures/Functions and procedures.py
@@ -0,0 +1,43 @@
+# Procedure with one parameter name
+def printName(name):
+    print(name)
+
+# Function with one parameter name
+def printNameFunc(name):
+    return name
+
+# Call the procedure
+printName("Sanveen")
+
+# Call the function and print returned value
+print(printNameFunc("Vichy"))
+
+# Call the function and store returned value in a variable
+returnedName = printNameFunc("Emby")
+print(returnedName)
+
+# Question 1 - Fill in the Gaps
+def add_numbers(a,b):
+    total = a + b
+    return total
+result = add_numbers(5,3)
+print(result)
+
+# Question 2 - Understanding Subprograms
+a) 
+Part of a larger program that does a specific job. 
+Subprograms can be reused.
+b) 
+A procedure does not return a value. 
+A function returns a single value.
+c) 
+Subprograms make code more maintainable. 
+Subprograms can be tested independently. 
+Developers can code subprograms independently.
+They make the code more readable.
+Makes it easier to spot errors in the code
+
+# Question 3 - Identifying Fuctions and Procedures
+
+
+# Question 4 - Writing a Function
